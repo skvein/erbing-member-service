63d128bfc3b864c4316dfeb177dd0e9851bb42fa
skevin <584473028@qq.com>
2019-05-24 18:29:51 +0800

    修改

---
+++ src/main/java/com/erbing/member/config/DruidConfiguration.java
@@ -0,0 +1,18 @@
+package com.erbing.member.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DruidConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druidDataSource() {
+        return new DruidDataSource();
+    }
+}
